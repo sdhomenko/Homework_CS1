f510ce54b5ea37d6ba02e89dfe615fa4e872c9e4
«Sergey <sdhomenko@gmail.com>
2022-04-16 17:53:46 +0300

    Task 1.05 Over

---
+++ Task_1.05/Program.cs
@@ -0,0 +1,18 @@
+﻿int N = 2;
+
+int i = 0;
+int k = 0;
+
+while (i <= N)
+{
+    k = -1 * N + i;
+    i ++;
+    Console.Write(k);
+    Console.Write(" ");
+}
+while (k < N)
+{
+    k++;
+    Console.Write(k);
+    Console.Write(" ");
+}
