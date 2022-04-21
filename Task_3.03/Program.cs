61de39aa8fbb7563e8938c0d2dc5528702091c5f
Sergey Homenko <sdhomenko@gmail.com>
2022-04-21 22:25:32 +0300

    Task 3.03

---
+++ Task_3.03/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int [] Grop1(int N)
+{
+    int[] array = new int[N];
+    int i = 1;
+    while (i < N)
+    {
+        array[i] = Math.Pow(new int[i], 2);
+        i++;
+    }
+    return array;
+}
+int N = 2;
+int [] res = 
+Console.WriteLine();
