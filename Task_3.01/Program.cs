a0f3fb44fa9321b8265b955e741c5c2c5c108478
Sergey Homenko <sdhomenko@gmail.com>
2022-04-21 22:23:34 +0300

    Task 3.01

---
+++ Task_3.01/Program.cs
@@ -0,0 +1,9 @@
+﻿int x = 1;
+int y = 0;
+
+int quarternum = 0;
+if (x > 0 && y > 0) quarternum = 1;
+if (x < 0 && y > 0) quarternum = 2;
+if (x < 0 && y < 0) quarternum = 3;
+if (x > 0 && y < 0) quarternum = 4;
+Console.WriteLine(quarternum);
