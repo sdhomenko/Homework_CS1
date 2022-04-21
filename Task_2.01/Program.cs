e47fa367f854e9223f07ed5ec5545939b8d5e22f
Sergey Homenko <sdhomenko@gmail.com>
2022-04-21 18:08:23 +0300

    Task_2.01

---
+++ Task_2.01/Program.cs
@@ -0,0 +1,11 @@
+﻿int GetNumber(int x)
+{
+    int a = x / 10;
+    int b = x % 10;
+    int max = a;
+    if (b > max) max = b;
+    return max;
+}
+int value = new Random().Next(10, 100);
+System.Console.WriteLine(value);
+System.Console.WriteLine(GetNumber(value));
