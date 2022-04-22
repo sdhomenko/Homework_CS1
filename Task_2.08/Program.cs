be470522122c3986029fffbb33e10a6da0d7e1c5
Sergey Homenko <sdhomenko@gmail.com>
2022-04-22 18:27:07 +0300

    Task 2.08 Over

---
+++ Task_2.08/Program.cs
@@ -0,0 +1,28 @@
+﻿int FindWeekend(int[] period, int find)
+{
+    int size = period.Length;
+    int index = 0;
+    int day = 0;
+    while (index < size)
+    {
+        if (period[index] == find)
+        {
+            day = find;
+        }
+
+        index++;
+    }
+    if (day == period[5] ^ day == period[6])
+    {
+        Console.WriteLine("ДА");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ");
+    }
+return day;
+}
+
+int[] week = { 1, 2, 3, 4, 5, 6, 7 };
+
+FindWeekend(week, 8);
