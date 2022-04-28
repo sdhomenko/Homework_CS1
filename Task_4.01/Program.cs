b4615782fa084ea668c30245f0d7ae70eb4d55fc
Sergey Homenko <sdhomenko@gmail.com>
2022-04-28 17:34:06 +0300

    Task_4.01

---
+++ Task_4.01/Program.cs
@@ -0,0 +1,60 @@
+﻿// Найти сумму чисел от 1 до А
+
+//Создаем массив из N элементов
+int[] CreateNums(int N)
+{
+    int[] result = new int[N];
+    return result;
+}
+
+//Метод заполнения массива числами от 1 до N по порядку 
+void SetNumsInOrder(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = i + 1;
+    }
+}
+
+//Метод заполнения произвольными числами
+void SetRandomNums(int[] numbers, int x, int y)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(x, y + 1);
+    }
+}
+
+//Метод который суммирует элементы
+int SumNums(int[] nums)
+{
+    int result = 0;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result = result + nums[i];
+    }
+    return result;
+}
+
+//Печать элементов с целью проверки
+void PrintNums(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        Console.Write($"{nums[i]} {" "}");
+    }
+}
+
+int N = 50;
+
+int[] nums = CreateNums(N);
+
+//SetRandomNums(nums, 1, 10);
+
+SetNumsInOrder(nums);
+
+PrintNums(nums);
+
+Console.WriteLine();
+
+Console.WriteLine(SumNums(nums));
