58c2e09ca59f61637fcd601ab7e8a935b917d566
Sergey Homenko <sdhomenko@gmail.com>
2022-05-06 17:06:23 +0300

    Homework 4 Over

---
+++ Task_4.03/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: 
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int InputData()
+{
+    Console.Write("Введите число: ");
+    string n = Console.ReadLine();
+    int N = Convert.ToInt32(n);
+    return N;
+}
+
+//Метод суммирования цифр числа
+int SumElNums(int N)
+{
+    int result = 0;
+    string num = Convert.ToString(N);
+    int j = num.Length;
+    for (int i = 0; i < j; i++)
+    {
+        result = result + (N / Convert.ToInt32(Math.Pow(10,i))) % 10;
+    }
+    return result;
+}
+
+//метод для вывода данных
+void PrintAnswer(int c)
+{
+  
+    Console.WriteLine($"{"Ответ: сумма цифр числа ="} {c}");
+
+
+}
+
+int N = InputData();
+
+PrintAnswer(SumElNums(N));
+
+
---
+++ Task_4.04/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 3:
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//Ввод колличества элементов массива
+int InputData()
+{
+    Console.Write("Введите колличество элементов для создания массива: ");
+    string n = Console.ReadLine();
+    int N = Convert.ToInt32(n);
+    return N;
+}
+
+//Создаем массив из N элементов
+int[] CreateNums(int N)
+{
+    int[] result = new int[N];
+    return result;
+}
+
+//Метод заполнения массива числами от 1 до N по порядку 
+void SetNumsInOrder(int[] nums)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] = i + 1;
+    }
+}
+
+//Метод заполнения произвольными числами от x до y
+void SetRandomNums(int[] numbers, int x, int y)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(x, y + 1);
+    }
+}
+
+//Печать элементов массива
+void PrintNums(int[] nums)
+{
+    Console.WriteLine("Ваш массив:");
+    for (int i = 0; i < nums.Length; i++)
+    {
+        
+        Console.Write($"{nums[i]} {" "}");
+    }
+}
+
+
+int N = InputData();
+
+int[] nums = CreateNums(N);
+
+SetRandomNums(nums, 1, 21);
+
+//SetNumsInOrder(nums);
+
+PrintNums(nums);
