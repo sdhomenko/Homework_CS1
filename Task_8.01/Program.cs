a62ef8246498590a63b6e813f49c532a3f121de7
Sergey Homenko <sdhomenko@gmail.com>
2022-05-25 19:35:12 +0300

    Homework 8

---
+++ Task_8.01/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 1: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+
+//Ввод количества строк и столбцов двухмерного массива
+(int, int) InputData()
+{
+
+    Console.Write("Введите количество строк: ");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    return (i, j);
+}
+
+//Создаем массив
+int[,] CreateMatrix(int i, int j)
+{
+    int[,] result = new int[i, j];
+    return result;
+}
+
+//Заполняем массив числами
+int[,] FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return matrix;
+}
+
+//Метод сортировки элементов строки по убыванию
+int[] SortElementRow(int[,] matrix, int i)
+{
+    int[] row = new int[matrix.GetLength(1)];
+    int[,] newmatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        row[j] = matrix[i, j];
+    }
+    Array.Sort(row);
+    Array.Reverse(row);
+    return row;
+}
+
+//Упорядочение елементов по строкам на убывание
+ int[,] SortElementMatrix(int[,] matrix)
+{
+    int[,] sorted = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int[] row = SortElementRow(matrix, i);
+         for (int j = 0; j < matrix.GetLength(1); j++)
+         {
+            sorted[i,j]=row[j];
+         }
+     }
+     return sorted;
+ }
+
+//Печать массива
+
+void PrintMatrix(int[,] matrix)
+{
+    const int cellWidth = 3;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],cellWidth}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Решение
+(int i, int j) = InputData();
+int[,] ourMatrix = FillMatrix(CreateMatrix(i, j));
+PrintMatrix(ourMatrix);
+PrintMatrix(SortElementMatrix(ourMatrix));
---
+++ Task_8.02/Program.cs
@@ -0,0 +1,100 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+//Ввод количества строк и столбцов двухмерного массива
+(int, int) InputData()
+{
+
+    Console.Write("Введите количество строк: ");
+    int i = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов: ");
+    int j = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine();
+    return (i, j);
+}
+
+//Создаем массив
+int[,] CreateMatrix(int i, int j)
+{
+    int[,] result = new int[i, j];
+    return result;
+}
+
+//Заполняем массив числами
+int[,] FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-9, 10);
+        }
+    }
+    return matrix;
+}
+
+//Метод сортировки элементов строки по убыванию
+int SumElementRow(int[,] matrix, int i)
+{
+    int sum = 0;
+    int[] row = new int[matrix.GetLength(1)];
+    int[,] newmatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        row[j] = matrix[i, j];
+    }
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum += row[j];
+    }
+    return sum;
+}
+
+//Упорядочение елементов по строкам на убывание
+int MinSumElementRow(int[,] matrix)
+{
+    int[] sumrow = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumrow[i] = SumElementRow(matrix, i);
+        }
+        //Console.WriteLine($"строка {i} - сумма элементов строки равна {sumrow[i]}");
+    }
+    int min = sumrow[0];
+    int a = 0;
+    for (int i = 0; i < sumrow.Length; i++)
+    {
+        if (sumrow[i] < min)
+        {
+            min = sumrow[i];
+            a = i;
+        }
+    }
+    return a;
+}
+
+//Печать массива
+
+void PrintMatrix(int[,] matrix)
+{
+    const int cellWidth = 3;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],cellWidth}");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Решение
+(int i, int j) = InputData();
+int[,] ourMatrix = FillMatrix(CreateMatrix(i, j));
+Console.WriteLine("Ваш массив:");
+PrintMatrix(ourMatrix);
+Console.WriteLine($"Cтрока с наименьшей суммой элементов - {MinSumElementRow(ourMatrix)}");
+Console.WriteLine();
